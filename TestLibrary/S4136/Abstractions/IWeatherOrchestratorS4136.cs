4f617467b1f87877da59bf2e3d86b56f04ddc2a5
Jannis Pohle <git@jannispohle.de>
2025-07-23 08:40:09 +0200

    feat: implement sonarqube code-smell S4136

---
+++ TestLibrary/S4136/Abstractions/IWeatherOrchestratorS4136.cs
@@ -0,0 +1,10 @@
+using TestLibrary.S4136.Models;
+
+namespace TestLibrary.S4136.Abstractions;
+
+public interface IWeatherOrchestrator
+{
+  Task<Result<List<WeatherModelCelsius>>> GetWeather(AccessMode mode, string? argument = null);
+
+  Task<Result<List<WeatherModelCelsius>>> GetWeather(AccessMode mode, string? argument, bool throwOnError);
+}
---
+++ TestLibrary/S4136/Models/AccessModeS4136.cs
@@ -0,0 +1,10 @@
+namespace TestLibrary.S4136.Models;
+
+public enum AccessMode
+{
+  None,
+  File,
+  Mock,
+  Database,
+  Web,
+}

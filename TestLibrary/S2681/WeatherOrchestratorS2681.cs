f2c71646f752acc8b5ff05956d90840eb4547e3e
Jannis Pohle <git@jannispohle.de>
2025-07-25 08:11:16 +0200

    Revert "fix(TestLibrary/S2681/WeatherOrchestratorS2681.cs): Auto-Resolved issue 'csharpsquid:S2681' - Multiline blocks should be enclosed in curly braces"
    This reverts commit 3c6febc94ca720d2230dc10d0091b0a4e3bb7d30.

--- TestLibrary/S2681/WeatherOrchestratorS2681.cs
+++ TestLibrary/S2681/WeatherOrchestratorS2681.cs
@@ -27,5 +27,35 @@ public class WeatherOrchestrator: IWeatherOrchestrator
   {
     try
     {
-      if (mode == AccessMode.None) {
-        _logger.LogWarning(
+      if (mode == AccessMode.None) _logger.LogWarning("Access mode is not specified"); return Result<List<WeatherModelCelsius>>.Failure(new ArgumentException("Access mode must be specified", nameof(mode)));
+
+      _logger.LogInformation("Getting weather from {AccessMode} with Argument: {Argument}", mode, argument);
+
+
+      var result = mode switch
+      {
+        AccessMode.File => await _fileAccessor.GetWeather(argument),
+        AccessMode.Mock => await _mockAccessor.GetWeather(argument),
+        AccessMode.Database => await GetWeatherDataFromDbAccessor(argument),
+        AccessMode.Web => await _apiAccessor.GetWeather(argument),
+        _ => throw new ArgumentOutOfRangeException(nameof(mode), mode, null)
+      };
+
+      _logger.LogInformation("Retrieved {Count} weather records", result.Count);
+
+      return Result<List<WeatherModelCelsius>>.Success(result);
+    }
+    catch (Exception e)
+    {
+      _logger.LogError(e, "Error retrieving weather data");
+
+      return Result<List<WeatherModelCelsius>>.Failure(e);
+    }
+  }
+
+  private async Task<List<WeatherModelCelsius>> GetWeatherDataFromDbAccessor(string? argument)
+  {
+    await _dbAccessor.OpenConnection(argument);
+    return await _dbAccessor.GetWeather(argument);
+  }
+}

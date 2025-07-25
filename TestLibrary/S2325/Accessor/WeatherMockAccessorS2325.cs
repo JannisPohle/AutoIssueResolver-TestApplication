5ed42953536f2bb4cce90b6ee143443809838273
Jannis Pohle <git@jannispohle.de>
2025-07-25 08:08:59 +0200

    Revert "fix(TestLibrary/S2325/Accessor/WeatherMockAccessorS2325.cs): Auto-Resolved issue 'csharpsquid:S2325' - Methods and properties that don't access instance data should be static"
    This reverts commit 91f8d2a4cb5720f1358f4313cd5d211d043c21ef.

--- TestLibrary/S2325/Accessor/WeatherMockAccessorS2325.cs
+++ TestLibrary/S2325/Accessor/WeatherMockAccessorS2325.cs
@@ -15,4 +15,24 @@ public class WeatherMockAccessor: WeatherAccessorBase
 
     if (!weather.Any())
     {
-      throw new DataNotFoundException(
+      throw new DataNotFoundException("No weather data available.");
+    }
+
+    return Task.FromResult(weather.ToList());
+  }
+
+  private IEnumerable<WeatherModelCelsius> GenerateWeatherData(string? argument)
+  {
+    var random = string.IsNullOrWhiteSpace(argument) ? new Random() : new Random(argument?.GetHashCode() ?? 0);
+
+    if (!int.TryParse(argument, out var count))
+    {
+      count = 10; // Default count if parsing fails
+    }
+
+    for (var i = 0; i < count; i++)
+    {
+      yield return new WeatherModelCelsius(random.Next(-20, 45));
+    }
+  }
+}

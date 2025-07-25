47a61a4697112dea356ba94aeab29e83e319f2e1
Jannis Pohle <git@jannispohle.de>
2025-07-25 08:13:00 +0200

    Revert "fix(TestLibrary/S4019/Accessor/WeatherMockAccessorS4019.cs): Auto-Resolved issue 'csharpsquid:S4019' - Base class methods should not be hidden"
    This reverts commit 1b638c9d1cd7a003f1c8126bd949470e0bae6c9d.

--- TestLibrary/S4019/Accessor/WeatherMockAccessorS4019.cs
+++ TestLibrary/S4019/Accessor/WeatherMockAccessorS4019.cs
@@ -15,4 +15,31 @@ public class WeatherMockAccessor: WeatherAccessorBase
 
     if (!weather.Any())
     {
-      throw new DataNotFoundException(
+      throw new DataNotFoundException("No weather data available.");
+    }
+
+    return Task.FromResult(weather.ToList());
+  }
+
+  public bool ValidateWeatherData(WeatherModelBase data)
+  {
+    var success = !string.IsNullOrEmpty(data.Unit);
+    Logger.LogTrace("Validate weather data : {Success}", success);
+    return success;
+  }
+
+  private static IEnumerable<WeatherModelCelsius> GenerateWeatherData(string? argument)
+  {
+    var random = string.IsNullOrWhiteSpace(argument) ? new Random() : new Random(argument?.GetHashCode() ?? 0);
+
+    if (!int.TryParse(argument, out var count))
+    {
+      count = 10; // Default count if parsing fails
+    }
+
+    for (var i = 0; i < count; i++)
+    {
+      yield return new WeatherModelCelsius(random.Next(-80, -45));
+    }
+  }
+}

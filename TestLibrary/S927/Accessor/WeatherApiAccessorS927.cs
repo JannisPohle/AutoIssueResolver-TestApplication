4eca1ad340d4e42b161ff0700b98506158b16d94
Jannis Pohle <git@jannispohle.de>
2025-07-26 05:49:11 +0200

    Revert "fix(TestLibrary/S927/Accessor/WeatherApiAccessorS927.cs): Auto-Resolved issue 'csharpsquid:S927' - Parameter names should match base declaration and other partial definitions"
    This reverts commit afeed2fd605f42757dced8f2682e5c1547bd534c.

--- TestLibrary/S927/Accessor/WeatherApiAccessorS927.cs
+++ TestLibrary/S927/Accessor/WeatherApiAccessorS927.cs
@@ -15,21 +15,58 @@ public sealed class WeatherApiAccessor(ILogger<WeatherApiAccessor> logger): Weat
 
   #region Methods
 
-  public override async Task<List<WeatherModelCelsius>> GetWeather(string? argument)
+  public override async Task<List<WeatherModelCelsius>> GetWeather(string? location)
   {
     try
     {
       var url = "http://localhost:31246/v1/api/weather";
 
-      if (!string.IsNullOrWhiteSpace(argument))
+      if (!string.IsNullOrWhiteSpace(location))
       {
-        url += $"?location={argument}";
+        url += $"?location={location}";
       }
 
       var response = _httpClient.GetFromJsonAsAsyncEnumerable<Models.External.WeatherApiModel>(url);
 
       if (response is null)
       {
-        Logger.LogWarning("No weather data found for argument: {Argument}", argument);
+        Logger.LogWarning("No weather data found for argument: {Argument}", location);
 
-        throw new DataNotFoundException($
+        throw new DataNotFoundException($"No weather data found for argument: {location}.");
+      }
+
+      var weatherData = new List<WeatherModelCelsius>();
+
+      await foreach (var weatherModel in response)
+      {
+        weatherData.Add(new WeatherModelCelsius((int) weatherModel.Temperature));
+      }
+
+      Logger.LogInformation("Found {WeatherDataCount} weather data for location {Argument}.", weatherData.Count, location);
+
+      return weatherData;
+    }
+    catch (Exception e)
+    {
+      Logger.LogWarning(e, "Failed to get weather data with argument: {Argument}", location);
+
+      throw new ConnectionFailedException($"Failed to connect to the weather API with argument: {location}.", e);
+    }
+  }
+
+  private void Dispose(bool disposing)
+  {
+    if (disposing)
+    {
+      _httpClient.Dispose();
+    }
+  }
+
+  public void Dispose()
+  {
+    Dispose(true);
+    GC.SuppressFinalize(this);
+  }
+
+  #endregion
+}

c9595ddac35af7a0c5064a5da1ad5d21f965394a
Jannis Pohle <git@jannispohle.de>
2025-07-26 10:09:13 +0200

    Revert "fix(TestLibrary/S927/Accessor/WeatherApiAccessorS927.cs): Auto-Resolved issue 'csharpsquid:S927' - Parameter names should match base declaration and other partial definitions"
    This reverts commit 32f747fdcd2595b81b34b1aef93f1cfb94a49fc2.

--- TestLibrary/S927/Accessor/WeatherApiAccessorS927.cs
+++ TestLibrary/S927/Accessor/WeatherApiAccessorS927.cs
@@ -1,1 +1,72 @@
-using System.Net.Http.Json;using Microsoft.Extensions.Logging;using TestLibrary.S927.Models;using TestLibrary.S927.Models.External;namespace TestLibrary.S927.Accessor;public sealed class WeatherApiAccessor(ILogger<WeatherApiAccessor> logger): WeatherAccessorBase(logger), IDisposable{  #region Members  private readonly HttpClient _httpClient = new();  #endregion  #region Methods  public override async Task<List<WeatherModelCelsius>> GetWeather(string? argument)  {    try    {      var url = "http://localhost:31246/v1/api/weather";      if (!string.IsNullOrWhiteSpace(argument))      {        url += $"?location={argument}";      }      var response = _httpClient.GetFromJsonAsAsyncEnumerable<Models.External.WeatherApiModel>(url);      if (response is null)      {        Logger.LogWarning("No weather data found for argument: {Argument}", argument);        throw new DataNotFoundException($"No weather data found for argument: {argument}.");      }      var weatherData = new List<WeatherModelCelsius>();      await foreach (var weatherModel in response)      {        weatherData.Add(new WeatherModelCelsius((int) weatherModel.Temperature));      }      Logger.LogInformation("Found {WeatherDataCount} weather data for location {Argument}.", weatherData.Count, argument);      return weatherData;    }    catch (Exception e)    {      Logger.LogWarning(e, "Failed to get weather data with argument: {Argument}", argument);      throw new ConnectionFailedException($"Failed to connect to the weather API with argument: {argument}.", e);    }  }  private void Dispose(bool disposing)  {    if (disposing)    {      _httpClient.Dispose();    }  }  public void Dispose()  {    Dispose(true);    GC.SuppressFinalize(this);  }  #endregion}
+using System.Net.Http.Json;
+using Microsoft.Extensions.Logging;
+using TestLibrary.S927.Models;
+using TestLibrary.S927.Models.External;
+
+namespace TestLibrary.S927.Accessor;
+
+public sealed class WeatherApiAccessor(ILogger<WeatherApiAccessor> logger): WeatherAccessorBase(logger), IDisposable
+{
+  #region Members
+
+  private readonly HttpClient _httpClient = new();
+
+  #endregion
+
+  #region Methods
+
+  public override async Task<List<WeatherModelCelsius>> GetWeather(string? location)
+  {
+    try
+    {
+      var url = "http://localhost:31246/v1/api/weather";
+
+      if (!string.IsNullOrWhiteSpace(location))
+      {
+        url += $"?location={location}";
+      }
+
+      var response = _httpClient.GetFromJsonAsAsyncEnumerable<Models.External.WeatherApiModel>(url);
+
+      if (response is null)
+      {
+        Logger.LogWarning("No weather data found for argument: {Argument}", location);
+
+        throw new DataNotFoundException($"No weather data found for argument: {location}.");
+      }
+
+      var weatherData = new List<WeatherModelCelsius>();
+
+      await foreach (var weatherModel in response)
+      {
+        weatherData.Add(new WeatherModelCelsius((int) weatherModel.Temperature));
+      }
+
+      Logger.LogInformation("Found {WeatherDataCount} weather data for location {Argument}.", weatherData.Count, location);
+
+      return weatherData;
+    }
+    catch (Exception e)
+    {
+      Logger.LogWarning(e, "Failed to get weather data with argument: {Argument}", location);
+
+      throw new ConnectionFailedException($"Failed to connect to the weather API with argument: {location}.", e);
+    }
+  }
+
+  private void Dispose(bool disposing)
+  {
+    if (disposing)
+    {
+      _httpClient.Dispose();
+    }
+  }
+
+  public void Dispose()
+  {
+    Dispose(true);
+    GC.SuppressFinalize(this);
+  }
+
+  #endregion
+}

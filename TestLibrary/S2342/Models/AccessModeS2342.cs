eaaf28732a89287e616a275033904fc13f77c521
Jannis Pohle <git@jannispohle.de>
2025-07-28 06:22:40 +0200

    Revert "fix(TestLibrary/S2342/Models/AccessModeS2342.cs): Auto-Resolved issue 'csharpsquid:S2342' - Enumeration types should comply with a naming convention"
    This reverts commit 4863d8b875c6f3c732120e30e4d13fab871e9c62.

--- TestLibrary/S2342/Models/AccessModeS2342.cs
+++ TestLibrary/S2342/Models/AccessModeS2342.cs
@@ -1,9 +1,9 @@
-`namespace TestLibrary.S2342.Models;
+namespace TestLibrary.S2342.Models;
 
 /// <summary>
 /// Contains the available access modes
 /// </summary>
-public enum AccessMode
+public enum ACM
 {
   None,
   File,
@@ -11,4 +11,3 @@ public enum AccessMode
   Database,
   Web,
 }
-`

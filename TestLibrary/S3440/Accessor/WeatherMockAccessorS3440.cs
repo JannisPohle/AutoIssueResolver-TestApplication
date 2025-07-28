b1cba0839bae44c008967b4dc876b5a8f86e86a4
Jannis Pohle <git@jannispohle.de>
2025-07-28 06:32:53 +0200

    Revert "fix(TestLibrary/S3440/Accessor/WeatherMockAccessorS3440.cs): Auto-Resolved issue 'csharpsquid:S3440' - Variables should not be checked against the values they're about to be assigned"
    This reverts commit b7eb91c339ff665e692c1a29a8e4f63b7594888f.

--- TestLibrary/S3440/Accessor/WeatherMockAccessorS3440.cs
+++ TestLibrary/S3440/Accessor/WeatherMockAccessorS3440.cs
@@ -1,4 +1,44 @@
-if (!int.TryParse(argument, out var count))
+using Microsoft.Extensions.Logging;
+using TestLibrary.S3440.Models;
+
+namespace TestLibrary.S3440.Accessor;
+
+public class WeatherMockAccessor: WeatherAccessorBase
+{
+  public WeatherMockAccessor(ILogger<WeatherMockAccessor> logger)
+    : base(logger)
+  { }
+
+  public override Task<List<WeatherModelCelsius>> GetWeather(string? argument)
+  {
+    var weather = GenerateWeatherData(argument);
+
+    if (!weather.Any())
+    {
+      throw new DataNotFoundException("No weather data available.");
+    }
+
+    return Task.FromResult(weather.ToList());
+  }
+
+  private static IEnumerable<WeatherModelCelsius> GenerateWeatherData(string? argument)
+  {
+    var random = string.IsNullOrWhiteSpace(argument) ? new Random() : new Random(argument?.GetHashCode() ?? 0);
+
+    if (!int.TryParse(argument, out var count))
+    {
+      count = 10; // Default count if parsing fails
+    }
+
+    var temperature = 0;
+    for (var i = 0; i < count; i++)
+    {
+      var next = random.Next(-20, 45);
+      if (next != temperature)
       {
-        count = 10; // Default count if parsing fails
+        temperature = next;
       }
+      yield return new WeatherModelCelsius(temperature);
+    }
+  }
+}

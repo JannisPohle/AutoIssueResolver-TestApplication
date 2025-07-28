35a8b0efca7b8f23c8bbe4f4ff9121b31a636435
Jannis Pohle <git@jannispohle.de>
2025-07-28 07:08:11 +0200

    Revert "fix(TestLibrary/S1699/Models/WeatherModelBaseS1699.cs): Auto-Resolved issue 'csharpsquid:S1699' - Constructors should only call non-overridable methods"
    This reverts commit d504522b7d26d2f10d126b6da3a28213f3285e6b.

--- TestLibrary/S1699/Models/WeatherModelCelsiusS1699.cs
+++ TestLibrary/S1699/Models/WeatherModelCelsiusS1699.cs
@@ -1,1 +1,21 @@
-public WeatherModelCelsius(int temperature) : base() { ... }
+using System.Text.Json.Serialization;
+
+namespace TestLibrary.S1699.Models;
+
+public class WeatherModelCelsius: WeatherModelBase
+{
+  public int Temperature { get; }
+
+  public override string Unit { get; protected set; }
+
+  [JsonConstructor]
+  public WeatherModelCelsius(int temperature)
+  {
+    Temperature = temperature;
+  }
+
+  protected override void SetUnit()
+  {
+    Unit = "Celsius";
+  }
+}

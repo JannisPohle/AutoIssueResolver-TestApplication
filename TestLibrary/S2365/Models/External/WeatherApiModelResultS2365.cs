7a897dfa35960df2a5d59646c795f6af12afae6f
Jannis Pohle <git@jannispohle.de>
2025-07-28 06:25:48 +0200

    Revert "fix(TestLibrary/S2365/Models/External/WeatherApiModelResultS2365.cs): Auto-Resolved issue 'csharpsquid:S2365' - Properties should not make collection or array copies"
    This reverts commit e9c7f7817e13826de7bc96604d439429bd96c928.

--- TestLibrary/S2365/Models/External/WeatherApiModelResultS2365.cs
+++ TestLibrary/S2365/Models/External/WeatherApiModelResultS2365.cs
@@ -1,1 +1,29 @@
-public List<WeatherApiModel> GetItems() => _items?.ToList() ?? [];
+using System.Text.Json.Serialization;
+
+namespace TestLibrary.S2365.Models.External;
+
+public class WeatherApiModelResult
+{
+  #region Members
+
+  private readonly List<WeatherApiModel> _items;
+
+  #endregion
+
+  #region Properties
+
+  //Ensure that the list cannot be modified, by creating a copy of the list
+  public List<WeatherApiModel> Items => _items?.ToList() ?? [];
+
+  #endregion
+
+  #region Constructors
+
+  [JsonConstructor]
+  public WeatherApiModelResult(List<WeatherApiModel>? items)
+  {
+    _items = items;
+  }
+
+  #endregion
+}

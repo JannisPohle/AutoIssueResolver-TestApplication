d7d6734bb3ab6f06e0c56f863e9714820abfe770
Jannis Pohle <git@jannispohle.de>
2025-07-28 07:21:26 +0200

    revert: "fix(TestLibrary/S2629/WeatherOrchestratorS2629.cs): Auto-Resolved issue 'csharpsquid:S2629' - Logging templates should be constant"
    This reverts commit 655f5cca4b2b201133030a40a05770c7944b36e7

--- TestLibrary/S2629/WeatherOrchestratorS2629.cs
+++ TestLibrary/S2629/WeatherOrchestratorS2629.cs
@@ -2,3 +2,63 @@ using Microsoft.Extensions.Logging;
 using TestLibrary.S2629.Abstractions;
 using TestLibrary.S2629.Accessor;
 using TestLibrary.S2629.Models;
+
+namespace TestLibrary.S2629;
+
+public class WeatherOrchestrator: IWeatherOrchestrator
+{
+  private readonly WeatherApiAccessor _apiAccessor;
+  private readonly WeatherDbAccessor _dbAccessor;
+  private readonly WeatherMockAccessor _mockAccessor;
+  private readonly WeatherFileAccessor _fileAccessor;
+  private readonly ILogger<WeatherOrchestrator> _logger;
+
+  public WeatherOrchestrator(WeatherFileAccessor fileAccessor, WeatherDbAccessor dbAccessor, WeatherApiAccessor apiAccessor, WeatherMockAccessor mockAccessor, ILogger<WeatherOrchestrator> logger)
+  {
+    _fileAccessor = fileAccessor;
+    _dbAccessor = dbAccessor;
+    _apiAccessor = apiAccessor;
+    _mockAccessor = mockAccessor;
+    _logger = logger;
+  }
+
+
+  public async Task<Result<List<WeatherModelCelsius>>> GetWeather(AccessMode mode, string? argument = null)
+  {
+    try
+    {
+      if (mode == AccessMode.None)
+      {
+        return Result<List<WeatherModelCelsius>>.Failure(new ArgumentException("Access mode must be specified", nameof(mode)));
+      }
+
+      _logger.LogInformation($"Getting weather from {mode} with Argument: {argument}");
+
+
+      var result = mode switch
+      {
+        AccessMode.File => await _fileAccessor.GetWeather(argument),
+        AccessMode.Mock => await _mockAccessor.GetWeather(argument),
+        AccessMode.Database => await GetWeatherDataFromDbAccessor(argument),
+        AccessMode.Web => await _apiAccessor.GetWeather(argument),
+        _ => throw new ArgumentOutOfRangeException(nameof(mode), mode, null)
+      };
+
+      _logger.LogInformation("Retrieved {Count} weather records", result.Count);
+
+      return Result<List<WeatherModelCelsius>>.Success(result);
+    }
+    catch (Exception e)
+    {
+      _logger.LogError(e, "Error retrieving weather data");
+
+      return Result<List<WeatherModelCelsius>>.Failure(e);
+    }
+  }
+
+  private async Task<List<WeatherModelCelsius>> GetWeatherDataFromDbAccessor(string? argument)
+  {
+    await _dbAccessor.OpenConnection(argument);
+    return await _dbAccessor.GetWeather(argument);
+  }
+}

1eeb2e4804982217fa1bde2757a28b0e53b99d80
Jannis Pohle <git@jannispohle.de>
2025-07-28 06:17:48 +0200

    Revert "fix(TestLibrary/S6672/Accessor/WeatherApiAccessorS6672.cs): Auto-Resolved issue 'csharpsquid:S6672' - Generic logger injection should match enclosing type"
    This reverts commit 8d88d97a69146dd2a2735e6dd3bb8063249a3f51.

--- TestLibrary/S6672/Accessor/WeatherApiAccessorS6672.cs
+++ TestLibrary/S6672/Accessor/WeatherApiAccessorS6672.cs
@@ -1,3 +1,79 @@
-public WeatherApiAccessor(ILogger<WeatherApiAccessor> logger)
+using System.Net.Http.Json;
+using Microsoft.Extensions.Logging;
+using TestLibrary.S6672.Models;
+
+namespace TestLibrary.S6672.Accessor;
+
+public sealed class WeatherApiAccessor: WeatherAccessorBase, IDisposable
+{
+  #region Members
+
+  private readonly HttpClient _httpClient = new();
+
+  #endregion
+
+  #region Constructors
+
+  public WeatherApiAccessor(ILogger<WeatherAccessorBase> logger)
     : base(logger)
   { }
+
+  #endregion
+
+  #region Methods
+
+  public override async Task<List<WeatherModelCelsius>> GetWeather(string? argument)
+  {
+    try
+    {
+      var url = "http://localhost:31246/v1/api/weather";
+
+      if (!string.IsNullOrWhiteSpace(argument))
+      {
+        url += $"?location={argument}";
+      }
+
+      var response = _httpClient.GetFromJsonAsAsyncEnumerable<Models.External.WeatherApiModel>(url);
+
+      if (response is null)
+      {
+        Logger.LogWarning("No weather data found for argument: {Argument}", argument);
+
+        throw new DataNotFoundException($"No weather data found for argument: {argument}.");
+      }
+
+      var weatherData = new List<WeatherModelCelsius>();
+
+      await foreach (var weatherModel in response)
+      {
+        weatherData.Add(new WeatherModelCelsius((int) weatherModel.Temperature));
+      }
+
+      Logger.LogInformation("Found {WeatherDataCount} weather data for location {Argument}.", weatherData.Count, argument);
+
+      return weatherData;
+    }
+    catch (Exception e)
+    {
+      Logger.LogWarning(e, "Failed to get weather data with argument: {Argument}", argument);
+
+      throw new ConnectionFailedException($"Failed to connect to the weather API with argument: {argument}.", e);
+    }
+  }
+
+  private void Dispose(bool disposing)
+  {
+    if (disposing)
+    {
+      _httpClient.Dispose();
+    }
+  }
+
+  public void Dispose()
+  {
+    Dispose(true);
+    GC.SuppressFinalize(this);
+  }
+
+  #endregion
+}

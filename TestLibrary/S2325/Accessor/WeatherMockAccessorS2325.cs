c1f8ddd7454a69125930cffdd05a9066949995a1
Jannis Pohle <git@jannispohle.de>
2025-07-28 07:41:21 +0200

    Revert "fix(TestLibrary/S2325/Accessor/WeatherMockAccessorS2325.cs): Auto-Resolved issue 'csharpsquid:S2325' - Methods and properties that don't access instance data should be static"
    This reverts commit 2f78bdbfe83dbe04ac7314047a46f3487b2be792.

--- TestLibrary/S2325/Accessor/WeatherMockAccessorS2325.cs
+++ TestLibrary/S2325/Accessor/WeatherMockAccessorS2325.cs
@@ -1,36 +1,38 @@
 using Microsoft.Extensions.Logging;
+using TestLibrary.S2325.Models;
 
 namespace TestLibrary.S2325.Accessor;
 
-public class WeatherMockAccessor : WeatherAccessorBase
+public class WeatherMockAccessor: WeatherAccessorBase
 {
-    public WeatherMockAccessor(ILogger<WeatherMockAccessor> logger)
-        : base(logger) { }
+  public WeatherMockAccessor(ILogger<WeatherMockAccessor> logger)
+    : base(logger)
+  { }
 
-    public override Task<List<WeatherModelCelsius>> GetWeather(string? argument)
+  public override Task<List<WeatherModelCelsius>> GetWeather(string? argument)
+  {
+    var weather = GenerateWeatherData(argument);
+
+    if (!weather.Any())
     {
-        var weather = GenerateWeatherData(argument);
-
-        if (!weather.Any())
-        {
-            throw new DataNotFoundException("No weather data available.");
-        }
-
-        return Task.FromResult(weather.ToList());
+      throw new DataNotFoundException("No weather data available.");
     }
 
-    private static IEnumerable<WeatherModelCelsius> GenerateWeatherData(string? argument)
+    return Task.FromResult(weather.ToList());
+  }
+
+  private IEnumerable<WeatherModelCelsius> GenerateWeatherData(string? argument)
+  {
+    var random = string.IsNullOrWhiteSpace(argument) ? new Random() : new Random(argument?.GetHashCode() ?? 0);
+
+    if (!int.TryParse(argument, out var count))
     {
-        var random = string.IsNullOrWhiteSpace(argument) ? new Random() : new Random(argument?.GetHashCode() ?? 0);
-
-        if (!int.TryParse(argument, out var count))
-        {
-            count = 10; // Default count if parsing fails
-        }
-
-        for (var i = 0; i < count; i++)
-        {
-            yield return new WeatherModelCelsius(random.Next(-20, 45));
-        }
+      count = 10; // Default count if parsing fails
     }
+
+    for (var i = 0; i < count; i++)
+    {
+      yield return new WeatherModelCelsius(random.Next(-20, 45));
+    }
+  }
 }

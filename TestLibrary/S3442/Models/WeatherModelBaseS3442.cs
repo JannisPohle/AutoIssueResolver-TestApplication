79bceee227b12cdce9d0ed6c789c116f4820d0bd
Jannis Pohle <git@jannispohle.de>
2025-07-28 06:54:00 +0200

    Revert "fix(TestLibrary/S3442/Models/WeatherModelBaseS3442.cs): Auto-Resolved issue 'csharpsquid:S3442' - "abstract" classes should not have "public" constructors"
    This reverts commit fc2d73e3b05ed11b70d5ce25d0ebf41ab465d937.

--- TestLibrary/S3442/Models/WeatherModelBaseS3442.cs
+++ TestLibrary/S3442/Models/WeatherModelBaseS3442.cs
@@ -1,5 +1,13 @@
-abstract class Base{
-    protected Base(){
-      //... 
-    }
+namespace TestLibrary.S3442.Models;
+
+public abstract class WeatherModelBase
+{
+  public DateOnly Date { get; }
+
+  public abstract string Unit { get; }
+
+  public WeatherModelBase()
+  {
+    Date = DateOnly.FromDateTime(DateTime.Now);
+  }
 }

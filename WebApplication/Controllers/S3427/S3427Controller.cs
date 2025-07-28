71cfe589818a915e281b94ed1d7ab98e3702e698
Jannis Pohle <git@jannispohle.de>
2025-07-28 07:15:37 +0200

    Revert "fix(TestLibrary/S3427/WeatherOrchestratorS3427.cs): Auto-Resolved issue 'csharpsquid:S3427' - Method overloads with default parameter values should not overlap"
    This reverts commit 20a4f343cb9cae6793ffd7f512def78cf8264d21.

--- WebApplication/Controllers/S3427/S3427Controller.cs
+++ WebApplication/Controllers/S3427/S3427Controller.cs
@@ -1,1 +1,57 @@
-public async Task<IActionResult> GetWeatherForecast([FromQuery] string? argument = null, [FromQuery] bool throwOnError = false)
+using Microsoft.AspNetCore.Mvc;
+using TestLibrary.S3427;
+using TestLibrary.S3427.Models;
+
+namespace WebApplication.Controllers.S3427;
+
+[ApiController]
+[Route("[controller]")]
+public class S3427Controller: ControllerBase
+{
+  #region Members
+
+  private readonly ILogger<S3427Controller> _logger;
+  private readonly WeatherOrchestrator _weatherOrchestrator;
+
+  #endregion
+
+  #region Constructors
+
+  public S3427Controller(ILogger<S3427Controller> logger, WeatherOrchestrator weatherOrchestrator)
+  {
+    _logger = logger;
+    _weatherOrchestrator = weatherOrchestrator;
+  }
+
+  #endregion
+
+  #region Methods
+
+  [HttpGet]
+  public async Task<IActionResult> GetWeatherForecast([FromQuery] string? argument = null, [FromQuery] bool throwOnError = false)
+  {
+    try
+    {
+      _logger.LogTrace("Get WeatherForecast");
+
+      var result = await _weatherOrchestrator.GetWeather(AccessMode.File, argument, throwOnError);
+
+      if (!result.IsSuccess)
+      {
+        _logger.LogError(result.Exception, "Failed to retrieve weather data");
+
+        return Ok(new List<WeatherModelCelsius>());
+      }
+
+      return Ok(result.Payload!);
+    }
+    catch (Exception e)
+    {
+      _logger.LogError(e, "Error reading WeatherForecast data");
+
+      return BadRequest();
+    }
+  }
+
+  #endregion
+}

1e2191c7cebba4dc5d924e4e33ffe4b2d0711a0b
Jannis Pohle <git@jannispohle.de>
2025-07-28 07:25:50 +0200

    Revert "fix(TestLibrary/S3398/Accessor/WeatherDbAccessorS3398.cs): Auto-Resolved issue 'csharpsquid:S3398' - "private" methods called only by inner classes should be moved to those classes"
    This reverts commit 6125290e5602dd6b12260dd8d1620a1f528e5aff.

--- TestLibrary/S3398/Accessor/WeatherDbAccessorS3398.cs
+++ TestLibrary/S3398/Accessor/WeatherDbAccessorS3398.cs
@@ -1,23 +1,30 @@
-public class WeatherDbAccessor: WeatherAccessorBase, IDisposable
+using System.Data;
+using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Logging;
+using TestLibrary.S3398.Models;
+
+namespace TestLibrary.S3398.Accessor;
+
+public sealed class WeatherDbAccessor: WeatherAccessorBase, IDisposable
 {
   #region Members
-  
+
   private readonly DatabaseManager _dbManager = new();
-  
+
   #endregion
-  
+
   #region Constructors
-  
+
   public WeatherDbAccessor(ILogger<WeatherDbAccessor> logger)
     : base(logger)
   {
     SQLitePCL.Batteries_V2.Init();
   }
-  
+
   #endregion
-  
+
   #region Methods
-  
+
   public async Task OpenConnection(string? argument)
   {
     try
@@ -32,24 +39,94 @@ public class WeatherDbAccessor: WeatherAccessorBase, IDisposable
     }
   }
 
-  private async Task<List<WeatherModelCelsius>> GetWeatherDataFromDbAccessor(string? argument)
+  private static void CloseConnection(SqliteConnection? connection)
   {
-    await _dbAccessor.OpenConnection(argument);
-    return await _dbAccessor.GetWeather(argument);
+    if (connection != null && connection.State != ConnectionState.Closed)
+    {
+      connection.Close();
+    }
+  }
+
+  private void Dispose(bool disposing)
+  {
+    if (disposing)
+    {
+      _dbManager?.Dispose();
+    }
+  }
+
+  public void Dispose()
+  {
+    Dispose(true);
+    GC.SuppressFinalize(this);
+  }
+
+  public override async Task<List<WeatherModelCelsius>> GetWeather(string? argument)
+  {
+    var cmd = _dbManager.CreateCommand();
+
+    var weatherList = new List<WeatherModelCelsius>();
+    cmd.CommandText = "SELECT Temperature FROM Weather";
+
+    await using var reader = await cmd.ExecuteReaderAsync();
+
+    while (await reader.ReadAsync())
+    {
+      var temperature = reader.GetInt32(0);
+      weatherList.Add(new WeatherModelCelsius(temperature));
+    }
+
+    return weatherList;
   }
 
   #endregion
 
-  
-  public class DatabaseManager
+  private sealed class DatabaseManager: IDisposable, IAsyncDisposable
   {
-    public async Task InitializeConnection(string? argument)
+    public void Dispose()
     {
-      // Implementation of InitializeConnection
+      CloseConnection(_connection);
+      _connection?.Dispose();
     }
 
-    private static void CloseDatabase()
+    public async ValueTask DisposeAsync()
     {
-      // Implementation of CloseDatabase
+      if (_connection != null)
+      {
+        CloseConnection(_connection);
+        await _connection.DisposeAsync();
+      }
+    }
+
+    private SqliteConnection? _connection;
+    
+    #region Static
+
+    private const string CONNECTION_STRING = "Data Source=TestFiles/weather.db";
+
+    #endregion
+
+    public async Task<SqliteConnection> InitializeConnection(string? argument)
+    {
+      _connection ??= new SqliteConnection(argument ?? CONNECTION_STRING);
+
+      if (_connection.State != ConnectionState.Open)
+      {
+        await _connection.OpenAsync();
+      }
+
+      return _connection;
+    }
+
+    public SqliteCommand CreateCommand()
+    {
+      if (_connection is null || _connection.State != ConnectionState.Open)
+      {
+        throw new InvalidOperationException("Database connection is not open. Call OpenConnection() before accessing the database.");
+      }
+      var cmd = _connection!.CreateCommand();
+
+      return cmd;
     }
   }
+}

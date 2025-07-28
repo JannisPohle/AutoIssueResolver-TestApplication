080d2acaba24e68a5a3c0eafd62e9ea4290e35b1
Jannis Pohle <git@jannispohle.de>
2025-07-28 07:47:16 +0200

    Revert "fix(WebApplication/Controllers/S6962/S6962Controller.cs): Auto-Resolved issue 'csharpsquid:S6962' - You should pool HTTP connections with HttpClientFactory"
    This reverts commit efea21447f0ad1f45e7c359ec2114f8f903c850f.

--- WebApplication/Controllers/S6962/S6962Controller.cs
+++ WebApplication/Controllers/S6962/S6962Controller.cs
@@ -1,52 +1,47 @@
-// File: Startup.cs
-public class Startup
-{
-    public void ConfigureServices(IServiceCollection services)
-    {
-        services.AddHttpClient();
-        // ...
-    }
-}
+using Microsoft.AspNetCore.Mvc;
+using TestLibrary.S6962.Models;
+
+namespace WebApplication.Controllers.S6962;
 
 [ApiController]
 [Route("[controller]")]
-public class S6962Controller : ControllerBase
+public class S6962Controller: ControllerBase
 {
-    private readonly ILogger<S6962Controller> _logger;
-    private readonly IHttpClientFactory _clientFactory;
+  private readonly ILogger<S6962Controller> _logger;
 
-    public S6962Controller(ILogger<S6962Controller> logger, IHttpClientFactory clientFactory)
+  public S6962Controller(ILogger<S6962Controller> logger)
+  {
+    _logger = logger;
+  }
+
+  [HttpGet("external")]
+  public async Task<IActionResult> GetWeatherForecast([FromQuery] string? argument = null)
+  {
+    try
     {
-        _logger = logger;
-        _clientFactory = clientFactory;
-    }
+      _logger.LogTrace("Get WeatherForecast");
 
-    [HttpGet("external")]
-    public async Task<IActionResult> GetWeatherForecast([FromQuery] string? argument = null)
+      using var httpClient = new HttpClient
+      {
+        BaseAddress = new Uri("http://localhost:31246/api/"),
+      };
+
+      var result = await httpClient.GetFromJsonAsync<List<WeatherModelCelsius>>("weather" + (string.IsNullOrWhiteSpace(argument) ? null : $"?location={argument}"));
+
+      if (result == null || result.Count == 0)
+      {
+        _logger.LogWarning("No data found from API endpoint for location: {Argument}", argument);
+
+        return NotFound();
+      }
+
+      return Ok(result);
+    }
+    catch (Exception e)
     {
-        try
-        {
-            _logger.LogTrace("Get WeatherForecast");
+      _logger.LogError(e, "Error reading WeatherForecast data");
 
-            var client = _clientFactory.CreateClient(); // Compliant (Basic usage)
-            client.BaseAddress = new Uri("http://localhost:31246/api/");
-
-            var result = await client.GetFromJsonAsync<List<WeatherModelCelsius>>("weather" + (string.IsNullOrWhiteSpace(argument) ? null : $"?location={argument}"));
-
-            if (result == null || result.Count == 0)
-            {
-                _logger.LogWarning("No data found from API endpoint for location: {{Argument}}", argument);
-
-                return NotFound();
-            }
-
-            return Ok(result);
-        }
-        catch (Exception e)
-        {
-            _logger.LogError(e, "Error reading WeatherForecast data");
-
-            return BadRequest();
-        }
+      return BadRequest();
     }
+  }
 }
